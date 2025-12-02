9518a383f1d1523785fd8d7782926e7802e43ae9
Fabi019 <fabi019@gmx.de>
2025-12-02 07:04:33 +0100

    day 2

---
+++ src/bin/day.rs
@@ -0,0 +1,16 @@
+aoc2025::main!("../../assets/day01.txt");
+
+fn part1(input: &str) -> u32 {
+    0
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+aoc2025::test!(
+    "\
+",
+    0,
+    0
+);
---
+++ src/bin/day02.rs
@@ -0,0 +1,53 @@
+aoc2025::main!("../../assets/day02.txt");
+
+fn part1(input: &str) -> u64 {
+    let ranges = input.lines().next().unwrap().split(",").map(|r| {
+        let (start, end) = r.split_once("-").unwrap();
+        (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap())
+    });
+
+    ranges.fold(0, |mut acc, (start, end)| {
+        'nextNumer:
+        for num in start..=end {
+            let s = num.to_string();
+            if s[0..s.len()/2] != s[s.len()/2..] {
+                continue 'nextNumer;
+            }
+            acc += num;
+        }
+        acc
+    })
+}
+
+fn part2(input: &str) -> u64 {
+    let ranges = input.lines().next().unwrap().split(",").map(|r| {
+        let (start, end) = r.split_once("-").unwrap();
+        (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap())
+    });
+
+    ranges.fold(0, |mut acc, (start, end)| {
+        'nextNumer:
+        for num in start..=end {
+            let s = num.to_string();
+            'nextPattern: for pat_len in 1..=s.len() / 2 {
+                let pattern = &s[0..pat_len];
+                for i in (pat_len..s.len()).step_by(pat_len) {
+                    if i + pat_len > s.len() || &s[i..i + pat_len] != pattern {
+                        continue 'nextPattern;
+                    }
+                }
+                acc += num;
+                continue 'nextNumer;
+            }
+        }
+        acc
+    })
+}
+
+aoc2025::test!(
+    "\
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+",
+    1227775554,
+    4174379265
+);
