544f582fdad9149f90b12a9bcbcebf27c75a79aa
Fabi019 <fabi019@gmx.de>
2025-12-08 18:40:18 +0100

    refactor day 8

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -2,7 +2,9 @@ use std::collections::HashSet;
 
 aoc2025::main!("../../assets/day08.txt");
 
-fn part1(input: &str) -> u32 {
+type PreparedInput = (Vec<(i64, i64, i64)>, Vec<(i64, (usize, usize))>);
+
+fn prepare_input(input: &str) -> PreparedInput {
     let positions = input
         .lines()
         .map(|line| {
@@ -15,131 +17,71 @@ fn part1(input: &str) -> u32 {
         })
         .collect::<Vec<_>>();
 
+    // Calculate the distances between all pairs
     let mut distances = Vec::new();
-
-    // Find the distances between all pairs
     for i in 0..positions.len() {
         for j in (i + 1)..positions.len() {
             let dx = (positions[i].0 - positions[j].0).abs();
             let dy = (positions[i].1 - positions[j].1).abs();
             let dz = (positions[i].2 - positions[j].2).abs();
-            distances.push(((i, j), f64::sqrt((dx * dx + dy * dy + dz * dz) as f64)));
+            distances.push((dx * dx + dy * dy + dz * dz, (i, j)));
         }
     }
 
     // Sort pairs by distance
-    distances.sort_unstable_by(|a, b| a.1.total_cmp(&b.1));
+    distances.sort_unstable_by_key(|d| d.0);
+    (positions, distances)
+}
 
-    let mut circuits: Vec<HashSet<_>> = Vec::new();
-
+fn part1(input: &str) -> usize {
+    let (_, distances) = prepare_input(input);
     let max_connections = if cfg!(test) { 10 } else { 1000 };
-    let mut connections = 0;
-    for ((i, j), d) in &distances {
-        println!(
-            "Connecting points {i}/{:?} to {j}{:?} distance: {d}",
-            positions[*i], positions[*j]
-        );
 
-        let group1 = circuits.iter().position(|g| g.contains(i));
-        let group2 = circuits.iter().position(|g| g.contains(j));
-
-        match (group1, group2) {
-            (Some(g1), Some(g2)) if g1 != g2 => {
-                let to_merge = circuits.remove(g2);
-                let new_index = if g2 > g1 { g1 } else { g1 - 1 };
-                circuits[new_index].extend(to_merge);
-            }
-            (Some(g1), None) => {
-                circuits[g1].insert(*j);
-            }
-            (None, Some(g2)) => {
-                circuits[g2].insert(*i);
-            }
-            (None, None) => {
-                let mut new_group = HashSet::new();
-                new_group.insert(*i);
-                new_group.insert(*j);
-                circuits.push(new_group);
-            }
-            _ => {}
-        }
-
-        connections += 1;
-        if connections >= max_connections {
-            break;
-        }
+    let mut circuits = Vec::new();
+    for (_, (i, j)) in distances.into_iter().take(max_connections) {
+        process_connection(i, j, &mut circuits);
     }
 
     // Sort by largest group
-    circuits.sort_unstable_by_key(|g| g.len());
-
-    // print connected groups
-    for group in &circuits {
-        println!("Connected group: {:?}", group);
-    }
-
+    circuits.sort_unstable_by_key(HashSet::len);
     circuits
         .iter()
         .rev()
         .take(3)
-        .fold(1, |acc, g| acc * g.len() as u32)
+        .map(HashSet::len)
+        .product::<usize>()
+}
+
+fn process_connection(i: usize, j: usize, circuits: &mut Vec<HashSet<usize>>) {
+    let group1 = circuits.iter().position(|g| g.contains(&i));
+    let group2 = circuits.iter().position(|g| g.contains(&j));
+
+    match (group1, group2) {
+        (Some(g1), Some(g2)) if g1 != g2 => {
+            let to_merge = circuits.remove(g2);
+            let new_index = if g2 > g1 { g1 } else { g1 - 1 };
+            circuits[new_index].extend(to_merge);
+        }
+        (Some(g1), None) => _ = circuits[g1].insert(j),
+        (None, Some(g2)) => _ = circuits[g2].insert(i),
+        (None, None) => {
+            let mut new_group = HashSet::new();
+            new_group.insert(i);
+            new_group.insert(j);
+            circuits.push(new_group);
+        }
+        _ => {}
+    };
 }
 
 fn part2(input: &str) -> i64 {
-    let positions = input
-        .lines()
-        .map(|line| {
-            let mut coords = line.split(',').map(|num| num.parse::<i64>().unwrap());
-            (
-                coords.next().unwrap(),
-                coords.next().unwrap(),
-                coords.next().unwrap(),
-            )
-        })
-        .collect::<Vec<_>>();
+    let (positions, distances) = prepare_input(input);
 
-    let mut distances = Vec::new();
-
-    // Find the distances between all pairs
-    for i in 0..positions.len() {
-        for j in (i + 1)..positions.len() {
-            let dx = (positions[i].0 - positions[j].0).abs();
-            let dy = (positions[i].1 - positions[j].1).abs();
-            let dz = (positions[i].2 - positions[j].2).abs();
-            distances.push(((i, j), f64::sqrt((dx * dx + dy * dy + dz * dz) as f64)));
-        }
-    }
-
-    // Sort pairs by distance
-    distances.sort_unstable_by(|a, b| a.1.total_cmp(&b.1));
-
-    let mut circuits: Vec<HashSet<_>> = Vec::new();
+    let mut circuits = Vec::new();
     let mut last_pair = None;
 
-    for ((i, j), _) in &distances {
-        let group1 = circuits.iter().position(|g| g.contains(i));
-        let group2 = circuits.iter().position(|g| g.contains(j));
-
-        match (group1, group2) {
-            (Some(g1), Some(g2)) if g1 != g2 => {
-                let to_merge = circuits.remove(g2);
-                let new_index = if g2 > g1 { g1 } else { g1 - 1 };
-                circuits[new_index].extend(to_merge);
-            }
-            (Some(g1), None) => {
-                circuits[g1].insert(*j);
-            }
-            (None, Some(g2)) => {
-                circuits[g2].insert(*i);
-            }
-            (None, None) => {
-                let mut new_group = HashSet::new();
-                new_group.insert(*i);
-                new_group.insert(*j);
-                circuits.push(new_group);
-            }
-            _ => {}
-        }
+    for (_, (i, j)) in distances {
+        process_connection(i, j, &mut circuits);
 
         // Check if all points are in one group
         if circuits.len() == 1 && circuits[0].len() == positions.len() {
@@ -149,7 +91,7 @@ fn part2(input: &str) -> i64 {
     }
 
     let (i, j) = last_pair.unwrap();
-    positions[*i].0 * positions[*j].0
+    positions[i].0 * positions[j].0
 }
 
 aoc2025::test!(
